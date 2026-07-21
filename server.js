116b00429a08112ce2c2a3dc385bfa8550f82705
Ghaizan <ghaizanaf2007@gmail.com>
2026-07-21 13:11:52 +0700

    feat: implement static file serving with MIME type support

--- server.js
+++ server.js
@@ -22,30 +22,53 @@ const routes = {
   "/contact": "contact.html",
 };
 
-const serverSsl = https.createServer(options, (req, res) => {
-  const fileName = routes[req.url];
+const mimeTypes = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".png": "image/png",
+  ".jpg": "image/jpg",
+  ".jpeg": "image/jpeg",
+  ".svg": "image/svg+xml",
+};
 
-  if (!fileName) {
-    res.writeHead(404, {
-      "content-type": "text/plain",
-    });
-    return res.end("404 Not Found");
-  }
-
-  const filePath = path.join(__dirname, "public", fileName);
-
+function sendFile(res, filePath, contentType) {
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      res.writeHead(500, {
-        "content-type": "text/plain",
+      res.writeHead(404, {
+        "content-Type": "text/plain",
       });
-      return res.end("Internal Server Error");
+      return res.end("404 File Not Found");
     }
     res.writeHead(200, {
-      "content-type": "text/html",
+      "content-Type": contentType,
     });
     res.end(data);
   });
+}
+
+const serverSsl = https.createServer(options, (req, res) => {
+  const url = req.url;
+  const fileName = routes[req.url];
+  if (
+    url.startsWith("/css/") ||
+    url.startsWith("/js/") ||
+    url.startsWith("/img/")
+  ) {
+    const filePath = path.join(__dirname, "public", url);
+    const ext = path.extname(filePath);
+    const contentType = mimeTypes[ext] || "application/octet-stream";
+    return sendFile(res, filePath, contentType);
+  }
+
+  if (!fileName) {
+    res.writeHead(404, {
+      "content-Type": "text/plain",
+    });
+    return res.end("404 Not Found");
+  }
+  const filePath = path.join(__dirname, "public", fileName);
+  sendFile(res, filePath, "text/html");
 });
 
 serverSsl.listen(PORT, () => {

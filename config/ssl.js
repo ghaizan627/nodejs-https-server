91467dfa56532266bd4f355179c381e3136647f1
Ghaizan <ghaizanaf2007@gmail.com>
2026-07-22 04:05:31 +0700

    Remove console.log statements from ssl.js
    Removed console.log statements for filename and dirname.

--- config/ssl.js
+++ config/ssl.js
@@ -3,9 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-console.log(__filename);
 const __dirname = path.dirname(__filename);
-console.log(__dirname);
 
 const options = {
   key: fs.readFileSync(path.join(__dirname, "..", "cert", "private-key.pem")),
